4997f3ea77aefb7402d1510344c4ae8ec3e6604d
Hasnat Ahmad <103493799+haxnat07@users.noreply.github.com>
2024-04-22 14:34:36 +0500

    Update settings.py

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*_@dvi^g6%!&dln=76=l1ok4=hl+$clzu&&^dz472z^!80r+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
